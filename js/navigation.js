4b52888bed1c5f5778e5017411b4fe9eb963f77b
ManuelBerasategui <manuelberasategui1@gmail.com>
2024-02-01 19:40:34 -0300

    fixed nosotros nav element

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,19 @@
-(function(){
-    const Nosotros = document.querySelector('.nosotros-item')
-
-    Nosotros.addEventListener('click', ()=>{
-        ToggleEvent(document.querySelector('.nav2-oculto-container').classList.add('nav2--show'))
-        
-    })
-}())
+(function () {
+    const Nosotros = document.querySelector('.nosotros-item');
+    const items = document.querySelector('.nav2-items')
+    const Nav2Container = document.querySelector('.nav2-oculto-container');
+  
+    Nosotros.addEventListener('mouseenter', () => {
+      Nav2Container.classList.add('nav2--show');
+      Nav2Container.style.height = 'auto'; // Ajusta la altura a 'auto' para medir el contenido
+      const height = Nav2Container.clientHeight + 'px';
+      Nav2Container.style.height = '0'; // Restablece la altura a cero
+      setTimeout(() => {
+        Nav2Container.style.height = height; // Aplica la altura medida para la animación
+      }, 0);
+    });
+  
+    Nosotros.addEventListener('mouseleave', () => {
+      Nav2Container.style.height = '0'; // Reduce la altura a cero al salir del hover
+    });
+  })();
